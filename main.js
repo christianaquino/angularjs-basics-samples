4c4310f4a3be7a05d35be43c23df59184ffc6af4
Christian Aquino <christianaquino@Christians-MacBook-Pro.local>
2016-04-17 19:47:18 -0300

    Added Promises and Services

--- main.js
+++ main.js
@@ -38,3 +38,53 @@ myApp.filter("NumberIsEven", function() {
 		return numStatus;
 	}
 });
+
+//Services
+myApp.factory('testFactory', function(){
+    return {
+        sayHello: function(text){
+            return "Factory says \"Hello " + text + "\"";
+        }  
+    }               
+});
+
+myApp.service('testService', function(){
+        this.sayHello = function(text){
+        return "Service says \"Hello " + text + "\"";
+    };        
+});
+
+myApp.controller("HelloController", ["$scope", "testService", "testFactory", function HelloController($scope, testService, testFactory) {
+		$scope.fromService = testService.sayHello("World");  
+		$scope.fromFactory = testFactory.sayHello("World");
+}]);
+
+//Promises
+myApp.controller("PromisesController", ["$scope", "$q", "$timeout", function($scope, $q, $timeout) {
+	var validateName = function validateName(name) {
+			var promise = $q(function(resolve, reject) {
+				$timeout(function() {
+		        	if (name === "John") {
+		        		resolve("I know him");
+		      		} else {
+		        		reject("I dont know him");
+		      		}
+				}, 1000);
+		      });
+
+			return promise;
+		};
+
+	validateName("Karen").then(function(successMessage) {
+		$scope.promiseKaren = successMessage;
+	}, function(reason) {
+		$scope.promiseKaren = reason;
+	});
+
+	validateName("John").then(function(successMessage) {
+		$scope.promiseJohn = successMessage;
+	}, function(reason) {
+		$scope.promiseJohn = reason;
+	});
+
+}]);
